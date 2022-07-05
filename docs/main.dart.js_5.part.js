74d5af8faad64c5d5f10522ef09fd23cac1abb0f
João Vitor <joaovitor.jsr@gmail.com>
2022-07-05 15:37:28 -0300

    new

--- docs/main.dart.js_5.part.js
+++
@@ -1,7 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
-b4i(){return new B.nO(null,null,null)}},B
-A=a.updateHolder(c[5],A)
-B=c[7]
-var z=a.updateTypes([])}
-$__dart_deferred_initializers__["CMTIwSbbfgx1x7VJJhZ04s5josg="] = $__dart_deferred_initializers__.current
