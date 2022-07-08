5d07858806858bb11a7086a43be173f0bcea68a4
João Vitor <joaovitor.jsr@gmail.com>
2022-07-08 13:45:34 -0300

    new

--- lib/widgetU/build_image.dart
+++ lib/widgetU/build_image.dart
@@ -1,4 +1,3 @@
-import 'package:ani_search/widgetU/build_widget.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
